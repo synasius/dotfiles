bed0e8452b31c78374636829168007248f5dd241
Federico Frenguelli <synasius@gmail.com>
2018-12-19 01:12:21 +0100

    Added some components when installing rust

--- rust/install.sh
+++ rust/install.sh
@@ -1,13 +1,17 @@
 #!/bin/zsh
 
 if (! (( $+commands[cargo] )) ); then
-    # Intall rust through rustup tool
+    # Install rust through rustup tool
     curl https://sh.rustup.rs -sSf | sh -s -- -y --no-modify-path
 
-    # this adds cargo binary to the current en
-    source $HOME/.cargo/env 
+    # this adds cargo binary to the current env
+    source $HOME/.cargo/env
+
+    # Installing some useful components
+    rustup component add rustfmt clippy rls
 fi
 
 if (! (( $+commands[rg] )) ); then
     cargo install ripgrep
 fi
+
